66d775ed2e32871d28287a08d4dacfcff8680154
BarnacleJones <benmjones@protonmail.com>
2022-07-14 15:34:47 +1200

    python database connection script added

---
+++ databaseTest.py
@@ -0,0 +1,17 @@
+
+import mysql.connector
+
+#open connection
+db = mysql.connector.connect(host ="localhost", user="root", password="you wish", database="sense_data")
+print(db)
+
+mycursor = db.cursor()
+mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+	print(x)
+
+commander = "INSERT INTO `data` (`TEMPERATURE`, `PRESSURE`, `HUMIDITY`, `TIMESTAMP`, `id`) VALUES ('111', '111', '111', '111', 3);"
+mycursor.execute(commander)
+db.commit()
+
